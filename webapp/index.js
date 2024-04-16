95e691d85735a84904e0079ad358cb671cfa6554
최한성(Choi Hanseong) <hanseong.choi@jmckor.com>
2024-04-16 15:49:07 +0900

    SAP UI5 Chap2 Steady..

--- webapp/index.js
+++ webapp/index.js
@@ -12,3 +12,13 @@ sap.ui.define([
     }).placeAt("content");
 
 });
+
+sap.ui.define([
+    "sap/ui/core/mvc/XMLView"
+], (XMLView) => {
+    "use strict";
+
+    XMLView.create({
+        viewName: "ui5.quickstart.App"
+    }).then((oView) => oView.placeAt("content"));
+});
