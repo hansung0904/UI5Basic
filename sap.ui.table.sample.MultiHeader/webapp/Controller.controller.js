905c5d7c4fbe1487cac38ead9f829d2df87c4a2f
최한성(Choi Hanseong) <hanseong.choi@jmckor.com>
2024-05-03 12:48:03 +0900

    Column 태그에 CSS 안먹는 문제 해결 -> Label 태그에 CSS Class 적용 후 해당 Label을 가지고 있는 부모에게 CSS적용 하도록 설정 / Row Span시 셀이 두개가 더 생기면서 화면 밀리는 현상 -> 해당 Element 가져오도록 조회 후 해당 Element 파괴

--- sap.ui.table.sample.MultiHeader/webapp/Controller.controller.js
+++ sap.ui.table.sample.MultiHeader/webapp/Controller.controller.js
@@ -25,18 +25,48 @@ sap.ui.define([
 
 		},
 		
-		/*
-			onAfterRendering: function() {
-		  var oColumn = this.getView().byId("supplierColumn");
-		  var oColumnHeader = oColumn.$().context
-		  oColumnHeader.setAttribute('rowspan', '2');
-		  console.log(oColumn.$().context);
+		
+		onAfterRendering: function() {
+		  let oColumn1 = this.getView().byId("supplierColumn");
+		  let oColumnHeader1 = oColumn1.$().context;
+		  oColumnHeader1.setAttribute('rowspan', '2');
+		  
 
-		  var oColumn = this.getView().byId("supplierColumn2");
-		  var oColumnHeader = oColumn.$().context
-		  oColumnHeader.setAttribute('rowspan', '2');
-	  },
-		*/
+		  let oColumn2 = this.getView().byId("supplierColumn2");
+		  let oColumnHeader2 = oColumn2.$().context;
+		  oColumnHeader2.setAttribute('rowspan', '2');
+		  
+		  /*
+			View Rendering시 컬럼이 두개 자동적으로 생기는 현상 발생
+			Element ID를 찾아서 삭제하는 식으로 구현
+			현재는 이게 최선이라는 피드백
+		  */
+		  let oElement1 = document.getElementById("__xmlview0--supplierColumn_1");
+			if (oElement1) {
+			    oElement1.parentNode.removeChild(oElement1);
+			}
 
+		  let oElement2 = document.getElementById("__xmlview0--supplierColumn2_1");
+			if (oElement2) {
+			    oElement2.parentNode.removeChild(oElement2);
+			}
+
+	  	},
+
+		//   onAfterRendering: function() {
+		// 	setTimeout(function() {
+		// 		var oTable = this.getView().byId("table1");
+		// 		var oHeader = oTable.$().find("thead")[0]; // 테이블의 헤더 요소 가져오기
+		
+		// 		var oColumnHeaders = oHeader.getElementsByTagName("th"); // 헤더 셀들 가져오기
+		// 		for (var i = 0; i < oColumnHeaders.length; i++) {
+		// 			var oHeaderCell = oColumnHeaders[i];
+		// 			oHeaderCell.setAttribute('rowspan', '2'); // rowspan 설정
+		// 		}
+		// 	}.bind(this), 0);
+		// },
+		
+		
+	
 	});
 });
